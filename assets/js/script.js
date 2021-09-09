b8451f08ec505581bf041943f8292f30f4fc970c
maplesyrupman <maplesyrupman@protonmail.com>
2021-09-08 22:25:36 -0400

    creat tasks using form input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,20 @@
-const buttonEl = document.querySelector('#save-task');
+const formEl = document.querySelector('#task-form');
 const tasksToDoEl = document.querySelector('#tasks-to-do');
 
-const createTaskHandler = () => {
+const createTaskHandler = (event) => {
+    event.preventDefault();
+    let taskNameInput = document.querySelector("input[name='task-name']").value;
+    let taskTypeInput = document.querySelector("select[name='task-type']").value;
+
     let listItemEl = document.createElement('li');
     listItemEl.className = 'task-item';
-    listItemEl.textContent = 'This is a new task';
+
+    let taskInfoEl = document.createElement('div');
+    taskInfoEl.className = 'task-info';
+    taskInfoEl.innerHTML = `<h3 class='task-name'>${taskNameInput}</h3><span class='task-type'>${taskTypeInput}</span>`;
+
+    listItemEl.appendChild(taskInfoEl);
     tasksToDoEl.appendChild(listItemEl);
 };
 
-buttonEl.addEventListener('click', createTaskHandler);
+formEl.addEventListener('submit', createTaskHandler);
