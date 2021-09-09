1927797d34cb0b82c9767382e2a9fd99542fbeb8
maplesyrupman <maplesyrupman@protonmail.com>
2021-09-08 22:45:46 -0400

    added ability to create custom tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,36 @@
 const formEl = document.querySelector('#task-form');
 const tasksToDoEl = document.querySelector('#tasks-to-do');
 
-const createTaskHandler = (event) => {
+const taskFormHandler = (event) => {
     event.preventDefault();
     let taskNameInput = document.querySelector("input[name='task-name']").value;
     let taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    if (!taskNameInput || !taskTypeInput) {
+        alert('You need to fill out the task form!');
+        return false;
+    }
+
+    formEl.reset();
+
+    let taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput,
+    };
+
+    createTaskEl(taskDataObj);
+};
+
+const createTaskEl = (taskDataObj) => {
     let listItemEl = document.createElement('li');
     listItemEl.className = 'task-item';
 
     let taskInfoEl = document.createElement('div');
     taskInfoEl.className = 'task-info';
-    taskInfoEl.innerHTML = `<h3 class='task-name'>${taskNameInput}</h3><span class='task-type'>${taskTypeInput}</span>`;
+    taskInfoEl.innerHTML = `<h3 class='task-name'>${taskDataObj.name}</h3><span class='task-type'>${taskDataObj.type}</span>`;
 
     listItemEl.appendChild(taskInfoEl);
     tasksToDoEl.appendChild(listItemEl);
-};
+}
 
-formEl.addEventListener('submit', createTaskHandler);
+formEl.addEventListener('submit', taskFormHandler);
